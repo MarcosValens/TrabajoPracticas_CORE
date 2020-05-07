56edbe9ee9887377ef4576d72a646442105a43b6
Christian Martínez Pizá <cmartinezp@esliceu.net>
2020-05-07 15:51:34 +0200

    ProfessorController añadido

---
+++ src/main/java/com/esliceu/core/controller/ProfessorController.java
@@ -0,0 +1,27 @@
+package com.esliceu.core.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfessorController {
+
+    @GetMapping("/cursos")
+    public ResponseEntity<String> getCursos() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/professor")
+    public ResponseEntity<String> getProfessor() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody String professorJson){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
