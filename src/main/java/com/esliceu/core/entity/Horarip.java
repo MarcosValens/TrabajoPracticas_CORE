08fd7605481f5e5a3f4a958912f488714da6f1d1
joanpacheco <joanantonipacheco@gmail.com>
2020-05-07 12:40:15 +0200

    Creadas las entitys de Horaria, Horarig y Horarip.

--- src/main/java/com/esliceu/core/entity/Horarip.java
+++ src/main/java/com/esliceu/core/entity/Horarip.java
@@ -1,4 +1,71 @@
 package com.esliceu.core.entity;
 
-public class Horarip {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+@Table(name = "horarip")
+public class Horarip implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codi")
+    private Long codi;
+
+    @Column(name = "durada")
+    private Long durada;
+
+    @Column(name = "hora")
+    private  Time hora;
+
+    @Column(name = "dia", columnDefinition = "DATE")
+    private Date dia;
+
+    @Column(name = "placa")
+    private Long placa;
+
+    public Horarip() {
+    }
+
+    public Long getCodi() {
+        return codi;
+    }
+
+    public void setCodi(Long codi) {
+        this.codi = codi;
+    }
+
+    public Long getDurada() {
+        return durada;
+    }
+
+    public void setDurada(Long durada) {
+        this.durada = durada;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+
+    public Date getDia() {
+        return dia;
+    }
+
+    public void setDia(Date dia) {
+        this.dia = dia;
+    }
+
+    public Long getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(Long placa) {
+        this.placa = placa;
+    }
 }
