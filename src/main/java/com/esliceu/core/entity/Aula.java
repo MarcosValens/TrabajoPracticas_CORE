5e34422b3ea36b2e22d354b825a1a419baafbcb8
joanpacheco <joanantonipacheco@gmail.com>
2020-05-07 12:23:22 +0200

    Creadas las entitys de Activitat, Alumne, Aula, Avaluacio.

--- src/main/java/com/esliceu/core/entity/Aula.java
+++ src/main/java/com/esliceu/core/entity/Aula.java
@@ -1,4 +1,35 @@
 package com.esliceu.core.entity;
 
-public class Aula {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "aula")
+public class Aula  implements Serializable {
+
+    @Id
+    @Column(name = "codi")
+    private Long codi;
+
+    @Column(name = "descripcio", length = 300)
+    private String descripcio;
+
+    public Aula() {
+    }
+
+    public Long getCodi() {
+        return codi;
+    }
+
+    public void setCodi(Long codi) {
+        this.codi = codi;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
 }
--- src/main/java/com/esliceu/core/entity/Avaluacio.java
+++ src/main/java/com/esliceu/core/entity/Avaluacio.java
@@ -1,4 +1,60 @@
 package com.esliceu.core.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "avaluacio")
 public class Avaluacio {
+
+    @Id
+    @Column(name = "codi")
+    private Long codi;
+
+    @Column(name = "descripcio", length = 300)
+    private String descripcio;
+
+    @Column(name = "data_inici", nullable = false, columnDefinition = "DATE")
+    private Date dataInici;
+
+    @Column(name = "data_fi", nullable = false, columnDefinition = "DATE")
+    private Date dataFi;
+
+    public Avaluacio() {
+    }
+
+    public Long getCodi() {
+        return codi;
+    }
+
+    public void setCodi(Long codi) {
+        this.codi = codi;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+
+    public Date getDataInici() {
+        return dataInici;
+    }
+
+    public void setDataInici(Date dataInici) {
+        this.dataInici = dataInici;
+    }
+
+    public Date getDataFi() {
+        return dataFi;
+    }
+
+    public void setDataFi(Date dataFi) {
+        this.dataFi = dataFi;
+    }
 }
