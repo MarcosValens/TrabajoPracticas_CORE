d091a02469981ca5820b37aece8fca84e282b5e3
joanpacheco <joanantonipacheco@gmail.com>
2020-05-08 15:05:00 +0200

    Commit para pull request.

--- src/main/java/com/esliceu/core/manager/XMLManager.java
+++ src/main/java/com/esliceu/core/manager/XMLManager.java
@@ -1,13 +1,15 @@
 package com.esliceu.core.manager;
 
-import org.omg.CORBA.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class XMLManager {
@@ -15,22 +17,36 @@ public class XMLManager {
     @Autowired
     private Environment environment;
 
-    public void readXML() {
+    @Autowired
+    private XmlParser xmlParser;
 
+    public boolean readXML(MultipartFile fileMultiPart) {
 
+        String fileName = "";
+        File file;
+        List<List> listaObjetosXML = new ArrayList<>();
+
+        try {
+
+            file = convertMultiPartToFile(fileMultiPart);
+            listaObjetosXML = xmlParser.parseXML("nombredel archivo");
+
+            // Leer cada objeto y con el manager correspondiente a cada objeto meter en la base de datos
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     private File convertMultiPartToFile(MultipartFile file) throws IOException {
 
-        /*
-         * Volvemos a juntar las partes del archivo ya que desde el
-         * cliente nos llega en partes
-         * */
-        File convFile = new File(path + file.getOriginalFilename());
+        // Juntamos las partes del archivo ya que cuando hace el upload se sube pòr partes
+        File convFile = new File(environment.getProperty("upload.directory") + file.getOriginalFilename());
         FileOutputStream fos = new FileOutputStream(convFile);
         fos.write(file.getBytes());
         fos.close();
         return convFile;
     }
-
 }
