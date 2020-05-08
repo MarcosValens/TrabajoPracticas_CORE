fe0f51bcc6768b9c871e9f89c93081d6e6a6ee4a
Miguel Angel Arrones <miguelaarrones@icloud.com>
2020-05-08 12:49:34 +0200

    Se han eliminado las classes que tienen que ver con horarios (Horaria, horarig y horarip) y se ha convertido la clase Sessio en una entidad.

--- src/main/java/com/esliceu/core/repository/HorarigRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esliceu.core.repository;
-
-import com.esliceu.core.entity.Horarig;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HorarigRepository extends CrudRepository<Horarig, Long> {
-}
