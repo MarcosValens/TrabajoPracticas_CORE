ae7257469f27e07c33c1e0e7b03a541e0b58fd09
Miguel Pons <miguelponsanchez@gmail.com>
2020-05-12 13:30:37 +0200

    Creado session manager y repository

---
+++ src/main/java/com/esliceu/core/manager/SessioManager.java
@@ -0,0 +1,16 @@
+package com.esliceu.core.manager;
+
+import com.esliceu.core.entity.Sessio;
+import com.esliceu.core.repository.SessioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessioManager {
+    @Autowired
+    SessioRepository sessioRepository;
+
+    public void createOrUpdate(Sessio sessio){
+        sessioRepository.save(sessio);
+    }
+}
