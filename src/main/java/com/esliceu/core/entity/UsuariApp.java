0ec70faae026d9373650e6682df7b87c02207c2f
joanpacheco <joanantonipacheco@gmail.com>
2020-05-18 20:26:47 +0200

    Afegida la relació amb el profesor.

--- src/main/java/com/esliceu/core/entity/UsuariApp.java
+++ src/main/java/com/esliceu/core/entity/UsuariApp.java
@@ -11,6 +11,10 @@ public class UsuariApp implements Serializable {
     @Column(name = "email", length = 30, nullable = false)
     private String email;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "professor_id", referencedColumnName = "codi")
+    private Professor professor;
+
     public String getEmail() {
         return email;
     }
