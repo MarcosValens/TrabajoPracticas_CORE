d0fa97e4ce497361c05a08c3b4661a53ea3bdcad
joanpacheco <joanantonipacheco@gmail.com>
2020-05-18 21:38:28 +0200

    Modificado el sample.properties.

--- src/main/java/com/esliceu/core/filter/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/esliceu/core/filter/CustomAuthenticationSuccessHandler.java
@@ -4,13 +4,13 @@ import com.esliceu.core.entity.UsuariApp;
 import com.esliceu.core.manager.UsuariAppManager;
 import com.esliceu.core.utils.JwTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -24,21 +24,26 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Autowired
     private UsuariAppManager usuariAppManager;
 
+    @Autowired
+    private Environment environment;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
         if (response.isCommitted()) {
             return;
         }
+
         DefaultOidcUser oidcUser = (DefaultOidcUser) authentication.getPrincipal();
         Map attributes = oidcUser.getAttributes();
         String email = (String) attributes.get("email");
         UsuariApp usuariApp = usuariAppManager.findByEmail(email);
-        String token = JwTokenUtil.generateToken(usuariApp);
+        String acces_token = JwTokenUtil.generateAcessToken(usuariApp);
+        String refresh_token = JwTokenUtil.generateRefreshToken(usuariApp);
 
-        System.out.println(email);
-
-        String redirectionUrl = UriComponentsBuilder.fromUriString(homeUrl)
-                .queryParam("auth_token", token)
+        String redirectionUrl = UriComponentsBuilder.fromUriString(environment.getProperty("REDIRECT_URL"))
+                .queryParam("acces_token", acces_token)
+                .queryParam("refresh_token", refresh_token)
                 .build().toUriString();
 
         System.out.println("Va a redirigir");
--- src/main/java/com/esliceu/core/utils/JwTokenUtil.java
+++ src/main/java/com/esliceu/core/utils/JwTokenUtil.java
@@ -4,24 +4,39 @@ import com.esliceu.core.entity.UsuariApp;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 import java.io.Serializable;
 import java.util.Date;
 
 public class JwTokenUtil implements Serializable {
 
-    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60;
-    public static final String SIGNING_KEY = "1234";
+    @Autowired
+    private static Environment environment;
 
-    public static String generateToken(UsuariApp usuariApp) {
+    public static String generateAcessToken(UsuariApp usuariApp) {
         Claims claims = Jwts.claims().setSubject(usuariApp.getEmail());
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuer("https://esliceu.com")
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY_SECONDS * 1000))
-                .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + environment.getProperty("ACCES_TOKEN_EXPIRE")))
+                .signWith(SignatureAlgorithm.HS256, environment.getProperty("SIGNING_KEY_TOKEN"))
                 .compact();
     }
+
+    public static String generateRefreshToken(UsuariApp usuariApp) {
+        Claims claims = Jwts.claims().setSubject(usuariApp.getEmail());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuer("https://esliceu.com")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + environment.getProperty("REFRESH_TOKEN_EXPIRE")))
+                .signWith(SignatureAlgorithm.HS256, environment.getProperty("SIGNING_KEY_TOKEN"))
+                .compact();
+    }
+
 }
