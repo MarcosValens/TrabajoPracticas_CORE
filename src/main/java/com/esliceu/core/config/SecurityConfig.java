97075e10a2618c200dee60e5edd05ba826ce2436
joanpacheco <joanantonipacheco@gmail.com>
2020-05-18 20:06:51 +0200

    Modificado el SecurityCOnfig y añadido la entidad para los usuarios de la aplicación.

--- src/main/java/com/esliceu/core/config/SecurityConfig.java
+++ src/main/java/com/esliceu/core/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.esliceu.core.config;
 
+import com.esliceu.core.manager.GoogleUserManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private GoogleUserManager googleUserManager;
 
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
 
-        httpSecurity
+        http
                 .antMatcher("/**").authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers(new String[]{"/"}).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(googleUserManager);
     }
 }
