b670f81e5349d1d42122a5361bb4aed663bbbef2
marcos <marcosvalens@gmail.com>
2020-05-19 15:38:45 +0200

    Añadido el extends de SpringBootServletInitializer en CoreApplication

--- src/main/java/com/esliceu/core/CoreApplication.java
+++ src/main/java/com/esliceu/core/CoreApplication.java
@@ -2,12 +2,19 @@ package com.esliceu.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CoreApplication {
+public class CoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CoreApplication.class);
     }
 
 }
