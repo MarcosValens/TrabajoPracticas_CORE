11194e1c143336c9ec3729e0bb837898503a3627
joanpacheco <joanantonipacheco@gmail.com>
2020-05-19 12:14:05 +0200

    Modificaciones para la optimización del TokenManager.

--- src/main/java/com/esliceu/core/manager/TokenManager.java
+++ src/main/java/com/esliceu/core/manager/TokenManager.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 @Service
 public class TokenManager implements Serializable {
@@ -18,17 +19,14 @@ public class TokenManager implements Serializable {
     @Autowired
     private static Environment environment;
 
-    public static final String SIGNING_KEY = "1234";
-
     public static String generateAcessToken(UsuariApp usuariApp) {
         Claims claims = Jwts.claims().setSubject(usuariApp.getEmail());
 
         return Jwts.builder()
                 .setClaims(claims)
-                .setIssuer("https://esliceu.com")
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()))
-                .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
+                .signWith(SignatureAlgorithm.HS256, Objects.requireNonNull(environment.getProperty("SIGNING_KEY_TOKEN")).getBytes())
                 .compact();
     }
 
@@ -37,17 +35,16 @@ public class TokenManager implements Serializable {
 
         return Jwts.builder()
                 .setClaims(claims)
-                .setIssuer("https://esliceu.com")
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()))
-                .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
+                .signWith(SignatureAlgorithm.HS256, Objects.requireNonNull(environment.getProperty("SIGNING_KEY_TOKEN")).getBytes())
                 .compact();
     }
 
     public String validateToken(String token) {
         try {
             Jwts.parser()
-                    .setSigningKey(environment.getProperty("SIGNING_KEY_TOKEN").getBytes())
+                    .setSigningKey(Objects.requireNonNull(environment.getProperty("SIGNING_KEY_TOKEN")).getBytes())
                     .parseClaimsJws(token)
                     .getBody();
             return "OK";
