c1dedebe30ac76a05d01eae4ecafc4416778798d
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-20 16:35:07 +0200

    hacemos pruebas

--- src/main/java/com/esliceu/core/controller/AlumneController.java
+++ src/main/java/com/esliceu/core/controller/AlumneController.java
@@ -21,6 +21,11 @@ public class AlumneController {
     @Autowired
     GrupManager grupManager;
 
+    @GetMapping("/hola")
+    public ResponseEntity<String> hola() {
+        return new ResponseEntity<>("HOLA", HttpStatus.OK);
+    }
+
     @GetMapping("/private/alumnos")
     public List<Alumne> getAllAlumnos() {
         return alumneManager.findAll();
