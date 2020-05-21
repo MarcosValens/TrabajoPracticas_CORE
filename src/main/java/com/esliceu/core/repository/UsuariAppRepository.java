b2fb6ecf1d10932989fca9769a7be799862116ba
Miguel Angel Arrones <miguelaarrones@icloud.com>
2020-05-21 12:57:48 +0200

    Arreglado fallo login usuarioapp

--- src/main/java/com/esliceu/core/repository/UsuariAppRepository.java
+++ src/main/java/com/esliceu/core/repository/UsuariAppRepository.java
@@ -7,6 +7,6 @@ public interface UsuariAppRepository extends CrudRepository<UsuariApp, String> {
 
     UsuariApp findByEmail(String email);
 
-    boolean findByEmailAndContrasenya(String email, String contrasenya);
+    UsuariApp findByEmailAndContrasenya(String email, String contrasenya);
 
 }
