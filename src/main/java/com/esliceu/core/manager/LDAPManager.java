5bd3570b0fbe3bf960721f890798d46bbf12b77d
Miguel Pons <miguelponsanchez@gmail.com>
2020-06-05 15:14:15 +0200

    Se suben los alumnos al ldap con contraseña encriptada en md5 y usernames y correo generado a partir de sus nombres. Falta probar.

--- src/main/java/com/esliceu/core/manager/LDAPManager.java
+++ src/main/java/com/esliceu/core/manager/LDAPManager.java
@@ -1,16 +1,19 @@
 package com.esliceu.core.manager;
 
+import com.esliceu.core.entity.Alumne;
 import com.esliceu.core.entity.Grup;
+import com.sun.mail.util.BASE64EncoderStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import java.util.Hashtable;
-import java.util.List;
+import java.security.MessageDigest;
+import java.util.*;
 
 import javax.naming.*;
 import javax.naming.directory.*;
+import javax.xml.bind.DatatypeConverter;
 
 @Service
 public class LDAPManager {
@@ -31,7 +34,7 @@ public class LDAPManager {
         System.out.println(this.context.getEnvironment());
     }
 
-    public void addUser() {
+    public void addUser(List<Alumne> alumnes) {
         try {
             BasicAttributes attrs = new BasicAttributes();
 
@@ -43,24 +46,66 @@ public class LDAPManager {
             classes.add("top");
             attrs.put(classes);
 
-            attrs.put("displayname", "ALUMNE JAVA");
-            attrs.put("gidnumber", "10001");
-            attrs.put("homedirectory", "/home/userjava");
-            attrs.put("l", "Localitat");
-            attrs.put("loginshell", "/bin/bash");
-            attrs.put("mail", "alumnejava@esliceu.net");
-            attrs.put("sn", "COGNOMSJAVA");
-            attrs.put("uid", "alumnejava");
-            attrs.put("uidnumber", "11772");
-            attrs.put("userpassword", "patata");
+            Set<String> alumnesSet = new HashSet<>();
+            for (Alumne alumne : alumnes) {
 
-            this.context.createSubcontext(this.url + "/cn=userjava,ou=users,ou=accounts,dc=esliceu,dc=com", attrs);
+                String displayName = alumne.getNom() + " " + alumne.getAp1() + " " + alumne.getAp2();
+                String gidNumber = alumne.getGrup().getCodi().toString();
+                String username = createUserName(alumne.getNom(), alumne.getAp1(), alumne.getAp2()).toLowerCase();
+                String mailLiceu = createEmailLiceu(alumne, alumnesSet).toLowerCase();
+                String cognoms = alumne.getAp1() + " " + alumne.getAp2();
+                Long uidNumberMod = alumne.getExpedient() + 10000;
+                String uidNumber = uidNumberMod.toString();
 
-            this.context.close();
+                attrs.put("displayname", displayName);
+                attrs.put("gidnumber", gidNumber);
+                attrs.put("homedirectory", "/home/" + username);
+                attrs.put("l", "Localitat");
+                attrs.put("loginshell", "/bin/bash");
+                attrs.put("mail", mailLiceu);
+                attrs.put("sn", cognoms);
+                attrs.put("uid", username);
+                attrs.put("uidnumber", uidNumber);
+                attrs.put("userpassword", cryptToMd5("esliceu2019"));
+
+                this.context.createSubcontext(this.url + "/cn=" + username + ",ou=users,ou=accounts,dc=esliceu,dc=com", attrs);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String createUserName(String nom, String cognom1, String cognom2) {
+        char primeraLletraNom = nom.charAt(0);
+        char primeraLletraCognom2 = cognom2.charAt(0);
+        return primeraLletraNom + cognom1 + primeraLletraCognom2;
+    }
+
+    private String createEmailLiceu(Alumne alumne, Set<String> alumnesSet) {
+        String nom = alumne.getNom();
+        String cognom1 = alumne.getAp1();
+        String cognom2 = alumne.getAp2();
+
+        String mailBase = nom + "." + cognom1 + "." + cognom2;
+        if (alumnesSet.contains(mailBase)) {
+            mailBase += alumne.getExpedient();
+        } else {
+            alumnesSet.add(mailBase);
+        }
+        return mailBase + "@esliceu.net";
+    }
+
+    public String cryptToMd5(String plainPassword) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(plainPassword.getBytes());
+            String base64 = Base64.getEncoder().encodeToString(digest.digest());
+            return "{MD5}" + base64;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void addGroup(List<Grup> grups) {
