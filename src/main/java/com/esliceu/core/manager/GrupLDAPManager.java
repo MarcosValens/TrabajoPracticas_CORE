cb17d5db3365af42a0081e42f87adc58d8d69f8b
Miguel Pons <miguelponsanchez@gmail.com>
2020-06-12 16:31:58 +0200

    Se inserten els membres als grups eliminant duplicats i actualitzant els membres

--- src/main/java/com/esliceu/core/manager/GrupLDAPManager.java
+++ src/main/java/com/esliceu/core/manager/GrupLDAPManager.java
@@ -2,10 +2,12 @@ package com.esliceu.core.manager;
 
 import com.esliceu.core.entity.Alumne;
 import com.esliceu.core.entity.Grup;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.naming.Context;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.*;
 import java.util.Hashtable;
@@ -16,6 +18,9 @@ public class GrupLDAPManager {
     private DirContext context;
     private String url;
     private Hashtable<String, String> environment;
+
+    @Autowired
+    private GrupManager grupManager;
 
     public GrupLDAPManager(@Value("${LDAP_URL}") String urlLdap, @Value("${LDAP_ADMIN}") String admin, @Value("${LDAP_PASSWORD}") String password) throws NamingException {
         this.environment = new Hashtable<>();
@@ -50,11 +55,35 @@ public class GrupLDAPManager {
 
     public void addMembers(List<Alumne> alumnes) throws NamingException {
         String base = "ou=groups,dc=esliceu,dc=com";
+        cleanGroups();
         for (Alumne alumne : alumnes) {
             ModificationItem[] mod = new ModificationItem[1];
             Attribute memberUidAttr = new BasicAttribute("memberUid", alumne.getUidNumberLDAP().toString());
             mod[0] = new ModificationItem(DirContext.ADD_ATTRIBUTE, memberUidAttr);
             context.modifyAttributes("cn=" + alumne.getGrup().getCurs().getDescripcio() + " " + alumne.getGrup().getNom() + "," + base, mod);
+        }
+    }
+
+    //TODO Intentar no eliminar tot per evitar duplicats i aplicar modificacions
+    private void cleanGroups() throws NamingException {
+        List<Grup> grups = grupManager.findAll();
+        for (Grup grup : grups) {
+            String base = "cn="+grup.getCurs().getDescripcio()+" "+grup.getNom()+",ou=groups,dc=esliceu,dc=com";
+
+            SearchControls sc = new SearchControls();
+            sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+            String filter = "(memberUid=*)";
+            System.out.println(base);
+            NamingEnumeration results = this.context.search(base, filter, sc);
+            while (results.hasMore()) {
+                SearchResult sr = (SearchResult) results.next();
+                Attributes attributes = sr.getAttributes();
+                Attribute attribute = attributes.get("memberUid");
+                ModificationItem[] mod = new ModificationItem[1];
+                mod[0] = new ModificationItem(DirContext.REMOVE_ATTRIBUTE, attribute);
+                context.modifyAttributes(base, mod);
+            }
         }
     }
 }
